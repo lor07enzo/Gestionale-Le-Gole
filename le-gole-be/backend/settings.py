3b69213df3f3b2109d23734b68c775b7a35f6f01
Lorenzo Pelone <pelone.lorenzo07@gmail.com>
2026-08-01 16:53:07 +0200

    Push aggiornamento valori e rotte

--- le-gole-be/backend/settings.py
+++ le-gole-be/backend/settings.py
@@ -181,7 +181,7 @@ ANYMAIL = {
     "RESEND_API_KEY": env("RESEND_API_KEY"),  # da variabile d'ambiente, mai hardcoded
 }
 
-DEFAULT_FROM_EMAIL = "delivered@resend.dev" # Email di test. Deve corrispondere al dominio verificato su Resend
+DEFAULT_FROM_EMAIL = "seniority55@outlook.it" # Email di test. Deve corrispondere al dominio verificato su Resend
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
